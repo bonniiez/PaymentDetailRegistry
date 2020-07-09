77db1b8fc2d2f9ddc617cd20c7f56dcafaac3ece
Bonnie <bonniezhengg@alumni.ubc.ca>
2020-07-09 12:31:57 -0700

    payment-detail/payment-detail-list component ang app

--- Angular7/src/app/app.component.ts
+++ Angular7/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css'] // we can add inline styles with styles:[]
 })
 export class AppComponent {
   title = 'Angular7';
